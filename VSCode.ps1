4ebb7d621b1575b13aa33737e47020a6b92553d5
Ashley McGlone <ashley.mcglone@microsoft.com>
2017-08-16 11:35:44 -0400

    Added file for VSCode demo points

---
+++ VSCode.ps1
@@ -0,0 +1,24 @@
+
+<#
+
+Language Mode
+
+CTRL ` for terminal
+
+CTRL SPACE for Intellisense
+
+F5 / F8
+
+Debugger / F9 / F10 / F11
+
+Paired punctuation
+
+Format Selection/Document
+
+Go To Definition / Find All References
+
+Search results and highlighting
+
+Script analyzer
+
+#>
