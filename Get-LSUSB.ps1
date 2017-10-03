5711cd610ae0bde63c8bae8c02be9442133d0ed6
Ashley McGlone <ashley.mcglone@microsoft.com>
2017-10-03 10:00:24 -0400

    Added command awareness for OSes without lsusb

--- Get-LSUSB.ps1
+++ Get-LSUSB.ps1
@@ -1,8 +1,12 @@
 Function Get-LSUSB {
-    lsusb | ForEach-Object {
-        $_ -match '^Bus (?<Bus>\d{3}) Device (?<Device>\d{3}): ID (?<ID>[0-9a-f:]{9}) (?<Name>.+)$' | Out-Null
-        $matches.Remove(0)
-        [PSCustomObject]$matches | Select-Object Bus, Device, ID, Name
+    If (Get-Command lsusb -ErrorAction SilentlyContinue) {
+        lsusb | ForEach-Object {
+            $_ -match '^Bus (?<Bus>\d{3}) Device (?<Device>\d{3}): ID (?<ID>[0-9a-f:]{9}) (?<Name>.+)$' | Out-Null
+            $matches.Remove(0)
+            [PSCustomObject]$matches | Select-Object Bus, Device, ID, Name
+        }
+    } Else {
+        Write-Warning "The command lsusb is not available on this operating system."
     }
 }
 
