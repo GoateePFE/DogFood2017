3ab293ff9505d284e13285d000f6c7bf22b4107c
Ashley McGlone <ashley.mcglone@microsoft.com>
2017-10-04 08:51:24 -0400

    Updated VSCode demo script

--- VSCode.ps1
+++ VSCode.ps1
@@ -13,12 +13,41 @@ Debugger / F9 / F10 / F11
 
 Paired punctuation
 
-Format Selection/Document
-
-Go To Definition / Find All References
-
 Search results and highlighting
 
-Script analyzer
-
 #>
+
+# Script analyzer
+dir /
+gps
+
+
+#Go To Definition / Find All References
+Function Get-Foo {
+    'here is your foo'
+}
+
+Get-Foo
+
+
+#region Expand me
+If ($true) {
+    #Expand braces
+}
+#endregion
+
+Get-Foo
+
+
+#Format Selection/Document
+If ($true) {
+'do this'
+'do that'}
+
+$MyHashTable = @{
+Name = 'Benny'
+Company='Octan'
+FavoriteDay = 'Taco Tuesday'
+HeightInCm=4
+}
+
